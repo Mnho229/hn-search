5f27eafa92bb0bed2fa03429aeb5d929f9ec6523
Mnho229 <michaelho3624@gmail.com>
2019-06-10 22:07:27 -0400

    Started work on detecting query

--- src/components/Feed/Comment/Comment.jsx
+++ src/components/Feed/Comment/Comment.jsx
@@ -6,6 +6,15 @@ class Comment extends PureComponent {
   _detectQuery(propName) {
     //TODO
     //REGEX to match in Text with query
+    if (this.props.query === '') {
+      return propName;
+    }
+    const regex = new RegExp(this.props.query, 'gi');
+    return propName.replace(regex, function(match, offset, string) {
+      return (
+        <span className="h-highlight">{match}</span>
+      );
+    })
   };
 
   render() {
@@ -14,14 +23,14 @@ class Comment extends PureComponent {
     //TODO: Format content of comment
     return (
       <div className="c-comment">
-        <h4 className="c-comment__title">{story_title}</h4>
+        <h4 className="c-comment__title">{this._detectQuery(story_title)}</h4>
         <ul className="c-comment__row">
           <li className="c-comment__stats">{author}</li>
           <li className="c-comment__stats">{date}</li>
           <li className="c-comment__stats">comments</li>
         </ul>
         <section className="c-comment__content"
-                 dangerouslySetInnerHTML={{__html:comment_text}} >
+          dangerouslySetInnerHTML={{__html: this._detectQuery(comment_text) }} >
         </section>
       </div>
     )
