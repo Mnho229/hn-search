f8479cdb496d9252ee97e5613ed934a816e7647c
Mnho229 <michaelho3624@gmail.com>
2019-06-14 18:40:18 -0400

    comment title based on query working.  content wip

--- src/components/Feed/Comment/Comment.jsx
+++ src/components/Feed/Comment/Comment.jsx
@@ -1,26 +1,25 @@
 import React, { PureComponent } from 'react';
+import reactStringReplace from 'react-string-replace';
 
 class Comment extends PureComponent {
 
   //Use arrow function in props field to add in arguments to parameters
   _detectQuery(propName) {
-    //TODO
-    //REGEX to match in Text with query
     if (this.props.query === '') {
       return propName;
     }
-    const regex = new RegExp(this.props.query, 'gi');
-    return propName.replace(regex, function(match, offset, string) {
-      return (
-        <span className="h-highlight">{match}</span>
-      );
-    })
+    const regex = new RegExp('(' + this.props.query + ')', 'gi');
+
+    return reactStringReplace(propName, regex, (match) => 
+      <span className="h-highlight">{match}</span>
+    );
   };
 
   render() {
     const { story_title, author, created_at_i, comment_text} = this.props;
     const date = new Date(created_at_i * 1000).toLocaleDateString("en-US");
     //TODO: Format content of comment
+    //May need to make use of split spring and isolate each one
     return (
       <div className="c-comment">
         <h4 className="c-comment__title">{this._detectQuery(story_title)}</h4>
